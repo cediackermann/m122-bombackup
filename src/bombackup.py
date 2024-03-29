05942425bc8b7b58c8b4b2a3fc687fc36e545ca4
cediackermann <ackermanncedi@gmail.com>
2024-03-29 14:36:57 +0100

    Moved files

--- src/bombackup.py
+++ src/bombackup.py
@@ -1,7 +1,29 @@
-
+import logging
+import toml
 
 def main():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='bombackup.log', encoding='utf-8', level=logging.DEBUG)
+    logger.debug('This message should go to the log file')
+    logger.info('So should this')
+    logger.warning('And this, too')
+    logger.error('And non-ASCII stuff, too, like Øresund and Malmö')
+    file=open("config.toml","w")
+    data_dict = {
+        "devices" : {
+            "name" : "Device-name",
+            "ip" : "Device-IP",
+            
+            "mailTo" : "Your-email-address",
+            "path" : "/path/to/folder",
+            
+        }
+    }
+            
+            
+    print(tomldata)
     print("Hello, World!")
+    
 
-    if __name__ == "__main__":
-        main() 
+if __name__ == "__main__":
+    main()
